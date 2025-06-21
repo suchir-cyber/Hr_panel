a21d74ec704ab5695597a73a054b181b86cfc112
suchir-cyber <suchirpandula@gmail.com>
2025-06-21 20:55:07 +0530

    updated app.css

--- hr_panel/src/App.js
+++ hr_panel/src/App.js
@@ -1,10 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* More routes: /employee/:id, /bookmarks, /analytics */}
+      </Routes>
+    </Router>
   );
 }
 
--- hr_panel/src/index.js
+++ hr_panel/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
